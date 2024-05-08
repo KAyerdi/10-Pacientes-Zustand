472df8ccb8cdfd46c26faab17cd13c5c099842e8
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2024-05-08 19:11:03 -0300

    agregando lista de pacientes

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,5 +1,28 @@
+import { usePatientStore } from "../store"
+
 export default function PatientList() {
+
+  const patients = usePatientStore(state => state.patients)
+
+  console.log(patients)
+
   return (
-    <div>PatientList</div>
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <p>Si hay Pacientes</p>
+      ) : (
+        <>
+        <h2 className="font-black text-3xl text-center">
+          No hay Pacientes
+        </h2>
+        <p className="text-xl mt-5 mb-10 text-center">
+          Comienza agregando pacientes {''}
+          <span className="text-indigo-600 font-bold">
+            y aparecerán en este lugar
+          </span>
+        </p>
+        </>
+      )}
+    </div>
   )
 }
